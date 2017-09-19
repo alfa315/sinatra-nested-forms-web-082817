159a43f3caae72d61351a2e8259e56a16f42fe8d
github username <github email address>
2017-09-19 10:30:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      erb :"pirates/show"
+    end
+
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
